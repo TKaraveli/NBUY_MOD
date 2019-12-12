8393b8580e72f5c64369284d9b528227bba305b2
TKaraveli <tayfunkaraveli@hotmail.com>
2019-12-12 20:00:28 +0300

    efdsf

---
+++ NBUY_MOD.Entities/News.cs
@@ -0,0 +1,25 @@
+﻿namespace NBUY_MOD.Entities
+{
+    public class News
+    {
+        public int Id { get; set; }
+
+        public string NewsName { get; set; }
+
+        public string NewsDescription { get; set; }
+
+        //NewsDetails
+
+        public string Title { get; set; }
+
+        public string NewsSubTitle { get; set; }
+
+        public string NewsContent { get; set; }
+
+        public string NewsImagePath { get; set; }
+
+        public Category Category { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
